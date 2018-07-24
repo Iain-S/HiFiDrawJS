099bad3f58d2946928cae623266a8047a715dc13
Iain <i.stenson@ic.ac.uk>
2018-07-24 18:45:17 +0100

    Have a working (though failing) functional test using selenium

---
+++ pyth/functional_tests.py
@@ -0,0 +1,67 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+import unittest
+import time
+
+
+class TestNewVisitor(unittest.TestCase):
+
+    def setUp(self):
+        self.browser = webdriver.Firefox()
+
+    def tearDown(self):
+        self.browser.quit()
+
+    def test_can_make_super_simple_diagram(self):
+        # Edith wants to make a super simple diagram to of her iPhone and active monitors.
+        # Someone gives her a link to our site
+        self.browser.get('http://localhost:8009/')
+
+        # She notices the page title and header mention to-do lists
+        self.assertIn('HiFiDraw', self.browser.title)
+
+        page_header = self.browser.find_element_by_tag_name('h1').text
+        self.assertIn('HiFi Draw', page_header)
+
+        # She notices a table row, which seems to want some source input
+        src_input_box = self.browser.find_element_by_id('id_src_1')
+        self.assertEqual(
+            src_input_box.get_attribute('placeholder'),
+            'source name'
+        )
+
+        # She notices a table row, which seems to want some destination input
+        src_input_box = self.browser.find_element_by_id('id_dst_1')
+        self.assertEqual(
+            src_input_box.get_attribute('placeholder'),
+            'destination name'
+        )
+
+        # # She types "Buy peacock feathers" into a text box (Edith's hobby
+        # # is tying fly-fishing lures)
+        # input_box.send_keys('Buy peacock feathers')
+        #
+        # # When she hits enter, the page updates, and now the page lists
+        # # "1: Buy peacock feathers" as an item in a to-do list table
+        # input_box.send_keys(Keys.ENTER)
+        # time.sleep(1)
+        # self.check_for_row_in_list_table('1: Buy peacock feathers')
+        #
+        # # There is still a text box inviting her to add another item. She
+        # # enters "Use peacock feathers to make a fly" (Edith is very
+        # # methodical)
+        # input_box = self.browser.find_element_by_id('id_new_item')
+        # input_box.send_keys('Use peacock feathers to make a fly')
+        # input_box.send_keys(Keys.ENTER)
+        # time.sleep(1)
+        #
+        # # The page updates again, and now shows both items on her list
+        # self.check_for_row_in_list_table('1: Buy peacock feathers')
+        # self.check_for_row_in_list_table('2: Use peacock feathers to make a fly')
+        #
+        # # Edith wonders whether the site will remember her list. Then she sees
+        # self.fail("We haven't finished yet!")
+
+
+if __name__ == '__main__':
+    unittest.main()
