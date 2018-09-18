917d3a0ee84abffa9974df9fa767c525a39fed57
Iain-S <iain.stenson1@gmail.com>
2018-09-18 22:36:04 +0100

    Updating to latest version of master

--- js/vis_scripts.js
+++ js/vis_scripts.js
@@ -1,5 +1,5 @@
 //var pressedKeys = {};
-/*global window, $, vis */
+/*global window, $, vis, document, event, console */
 window.pressedKeys = {};
 
 $(document.body).keydown(function (evt) {
@@ -41,7 +41,7 @@ function countBodyRows(tableBody) {
 }
 
 
-function makeConnectorMenu() {
+function makeConnectorMenu(value) {
     // Function-level strict mode syntax
     'use strict';
 
@@ -55,7 +55,8 @@ function makeConnectorMenu() {
         {val: 'TRS<>XLR', text: 'TRS<>XLR'},
         {val: 'XLR<>XLR', text: 'XLR<>XLR'},
         {val: 'XLR<>RCA', text: 'XLR<>RCA'},
-        {val: 'XLR<>TRS', text: 'XLR<>TRS'}
+        {val: 'XLR<>TRS', text: 'XLR<>TRS'},
+        {val: 'speaker cable', text: 'speaker cable'}
         //{val : 3, text: 'spare<>spare'},
         //{val : 3, text: 'Wireless'},
     ];
@@ -65,6 +66,8 @@ function makeConnectorMenu() {
     $(arr).each(function () {
         sel.append($("<option>").attr('value',this.val).text(this.text));
     });
+
+    sel.val(value);
 
     return sel;
 }
@@ -81,7 +84,7 @@ function makeSourceBox(value) {
     element.setAttribute("type", "text");
     element.setAttribute("name", "Test Name");
     element.setAttribute("size", "7");
-    element.setAttribute("id", "id_src_1");
+    //element.setAttribute("id", "id_src_1");
     element.setAttribute("placeholder", "source");
     //element.setAttribute("style", "height: 10%");
     element.setAttribute("style", "padding: 0.4rem 0.4rem");
@@ -107,7 +110,7 @@ function makeDestinationBox(value) {
     element.setAttribute("type", "text");
     element.setAttribute("name", "Test Name");
     element.setAttribute("size", "7");
-    element.setAttribute("id", "id_dst_1");
+    //element.setAttribute("id", "id_dst_1");
     element.setAttribute("placeholder", "destination");
     //element.setAttribute("style", "height: 10%");
     element.setAttribute("style", "padding: 0.4rem 0.4rem");
@@ -134,7 +137,6 @@ function makeDeleteButton() {
     element.setAttribute("value", "Delete");
     element.setAttribute("style", "padding: 8px 8px");
 
-
     let jqe = $(element);
 
     jqe.click(
@@ -148,7 +150,7 @@ function makeDeleteButton() {
 }
 
 
-function addRow(tableBody, source_val=null, dest_val=null) {
+function addRow(tableBody, source_val = null, dest_val = null, conn_val = null) {
 
     //let currentRows = countBodyRows(tableBody);
 
@@ -160,7 +162,7 @@ function addRow(tableBody, source_val=null, dest_val=null) {
     makeSourceBox(source_val).appendTo(srcCell);
 
     let conCell = newRow.insertCell(1);
-    makeConnectorMenu().appendTo(conCell);
+    makeConnectorMenu(conn_val).appendTo(conCell);
 
     let dstCell = newRow.insertCell(2);
     makeDestinationBox(dest_val).appendTo(dstCell);
@@ -287,22 +289,22 @@ function graphFromTable(tableObj) {
     $.each(tableRows, function (ignore, value) {
         let tableRow = $(value);
         if (rowIsValid(tableRow)) {
-            // get source label
+            // get source
             let src_td = tableRow.children('td').eq(0);
 
-            // if source label is not in nodes, add it
+            // if source is not in nodes already, add it
             let src_id = addNodeFromCell(src_td, nodes);
 
-            // get dest label
+            // get dest
             let dst_td = tableRow.children('td').eq(2);
 
-            // if dest label is not in nodes, add it
+            // if dest is not in nodes already, add it
             let dst_id = addNodeFromCell(dst_td, nodes);
 
             // find label from drop-down
             let conn_td = tableRow.children('td').eq(1);
             let conn_label = conn_td.children('select').first().val();
-            console.log(conn_label);
+
             // add edge
             edges.push({from: src_id,
                         to: dst_id,
@@ -326,6 +328,10 @@ function redraw(drawingArea, tableObj) {
 
     let graph = graphFromTable(tableObj);
 
+    let link_url = window.location.origin + window.location.pathname + "?serialised=" + serialiseGraph(graph);
+    $('#id_export_link').text(link_url);
+    //console.log(link_url);
+
     let vis_nodes = new vis.DataSet(graph.nodes);
     let vis_edges = new vis.DataSet(graph.edges);
     let vis_container = drawingArea[0];
@@ -336,14 +342,16 @@ function redraw(drawingArea, tableObj) {
                            font: {size: 50,
                                   face: 'Patrick Hand SC, arial'}
                            //https://fonts.googleapis.com/css?family=Neucha|Patrick+Hand+SC
-                           },
-                       edges: {
-                           length: 100
-                           },
+                       },
+                       edges: {length: 100,
+                               font: {size: 30,
+                                      face: 'Patrick Hand SC, arial'}
+                       },
                        // ,nodes: {shadow: true},
                        // edges: {shadow: true}
                        layout: {
-                           hierarchical: {direction: 'LR'}
+                           hierarchical: {direction: 'LR',
+                                          levelSeparation: 300}
                            }
                        };
 
@@ -351,7 +359,14 @@ function redraw(drawingArea, tableObj) {
                     edges: vis_edges};
 
     // draw the thing
-    new vis.Network(vis_container, vis_data, vis_options);
+    let network = new vis.Network(vis_container, vis_data, vis_options);
+
+    network.on("afterDrawing", function (ignore) {
+        let download_link = document.getElementById('id_download');
+        let canvas = document.getElementsByTagName('canvas')[0];
+        download_link.setAttribute('download', 'HiFiDraw.png');
+        download_link.setAttribute('href', canvas.toDataURL("image/png").replace("image/png", "image/octet-stream"));
+    });
 }
 
 
@@ -387,6 +402,7 @@ function deleteRowFromID(tableID, idx) {
     redraw(drawingArea, theTable);
 }
 
+
 function addSampleData(sourceTableID) {
     // Function-level strict mode syntax
     'use strict';
@@ -395,10 +411,95 @@ function addSampleData(sourceTableID) {
 
     let tableBody = tableObj.children('tbody').first();
 
-    addRow(tableBody, 'phone', 'amp');
+    addRow(tableBody, 'phone', 'amp', 'XLR<>XLR');
     addRow(tableBody, 'amp', 'speakers');
 
     let drawingArea = $('#drawing_div');
 
     redraw(drawingArea, tableObj);
+}
+
+
+function serialiseGraph(graphData) {
+    // Function-level strict mode syntax
+    'use strict';
+    return JSON.stringify(graphData);
+}
+
+
+function deserialiseGraph(serialisedGraph) {
+    // Function-level strict mode syntax
+    'use strict';
+    return JSON.parse(serialisedGraph);
+}
+
+
+/* Get query parameters from the URL
+   e.g. www.my-site.com?something=a_thing&what=why
+        will return a dict with something and what as keys
+   You can call it like this getQueryParams(document.location.search)*/
+function getQueryParams(qs) {
+    // Function-level strict mode syntax
+    'use strict';
+
+    qs = qs.split('+').join(' ');
+
+    let params = {},
+        tokens,
+        re = /[?&]?([^=]+)=([^&]*)/g;
+
+    do {
+        tokens = re.exec(qs);
+        if (tokens) {
+            params[decodeURIComponent(tokens[1])] = decodeURIComponent(tokens[2]);
+        } else {
+            break;
+        }
+    } while (true);
+
+    return params;
+}
+
+
+function addDataFromURL(serialisedData, sourceTable) {
+    console.log(deserialiseGraph(serialisedData));
+}
+
+
+function setUpPage(sourceTable) {
+    // Function-level strict mode syntax
+    'use strict';
+    let query_params = getQueryParams(document.location.search);
+
+    if (query_params.hasOwnProperty('serialised')) {
+        addDataFromURL(query_params.serialised, sourceTable);
+    } else {
+        // Add a first row to save the user a click
+        addSampleData(sourceTable);
+    }
+}
+
+
+function copyToClipboard () {
+    // Function-level strict mode syntax
+    'use strict';
+    let str = $('#id_export_link').text();
+    console.log(str);
+    const el = document.createElement('textarea');  // Create a <textarea> element
+    el.value = str;                                 // Set its value to the string that you want copied
+    el.setAttribute('readonly', '');                // Make it readonly to be tamper-proof
+    el.style.position = 'absolute';
+    el.style.left = '-9999px';                      // Move outside the screen to make it invisible
+    document.body.appendChild(el);                  // Append the <textarea> element to the HTML document
+    const selected =
+        document.getSelection().rangeCount > 0      // Check if there is any content selected previously
+        ? document.getSelection().getRangeAt(0)     // Store selection if found
+        : false;                                    // Mark as false to know no selection existed before
+    el.select();                                    // Select the <textarea> content
+    document.execCommand('copy');                   // Copy - only works as a result of a user action (e.g. click events)
+    document.body.removeChild(el);                  // Remove the <textarea> element
+    if (selected) {                                 // If a selection existed before copying
+        document.getSelection().removeAllRanges();  // Unselect everything on the HTML document
+        document.getSelection().addRange(selected); // Restore the original selection
+    }
 }
